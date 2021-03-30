57a4c7b1357089d71bbb7d5f456f74184de6774f
notThere0815 <klappstuhl@schoschonen.at>
2021-03-30 23:13:20 +0200

    version 0.1 already done

--- Affirmations.Spectre/Program.cs
+++ Affirmations.Spectre/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Net.Http;
+using Spectre.Console;
 
 namespace Affirmations.Spectre
 {
     class Program
     {
+        private readonly HttpClient _client;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var p = new Program();
+        }
+
+        public Program()
+        {
+            _client = new HttpClient
+            {
+                BaseAddress = new Uri("https://www.affirmations.dev/")
+            };
+            while (true)
+            {
+                if (!AnsiConsole.Confirm("Need some [bold green]Affirmation[/]?"))
+                    if (AnsiConsole.Confirm("Do you want to [bold red]quit[/]:sad_but_relieved_face:"))
+                        return;
+                    else 
+                        continue;
+                var jsonResponse = _client.GetStringAsync("").Result;
+                AnsiConsole.Render(new Markup($"[bold red]{jsonResponse}\n[/]"));
+            }
         }
     }
 }
